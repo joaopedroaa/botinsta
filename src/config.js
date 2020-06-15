b849ea0c4f558c61909db1862586907da650e500
João Pedro Alvarenga <joaopedroaats@pm.me>
2020-06-15 13:41:01 -0300

    feat: create login action

---
+++ src/config.js
@@ -0,0 +1,10 @@
+const config = {
+  subreddit: ["memes", "funny", "lol"],
+  browser: { chrome: "/opt/google/chrome/chrome" },
+  userAgent:
+    "Mozilla/5.0 (Linux; Android 8.0; Pixel 2 Build/OPD3.170816.012) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Mobile Safari/537.36",
+  userAgent2:
+    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4168.2 Safari/537.36",
+};
+
+export default config;
---
+++ src/selectors.js
@@ -0,0 +1,13 @@
+export default {
+  login: "_2hvTZ.pexuQ.zyHYP",
+  loginPhone: "sqdOP.L3NKy.y3zKF",
+  post:
+    "#react-root > section > nav.NXc7H.f11OC > div > div > div.KGiwt > div > div > div.q02Nz._0TPg",
+  next:
+    "#react-root > section > div.Scmby > header > div > div.mXkkY.KDuQp > button",
+  expand:
+    "#react-root > section > div.gH2iS > div.N7f6u.Bc-AD > div > div > div > button.pHnkA",
+  wait:
+    "#react-root > section > nav.NXc7H.f11OC > div > div > div.KGiwt > div > div > div:nth-child(1) > a > span",
+
+}
