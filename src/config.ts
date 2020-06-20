2efb3cd7857395110e9281088b26d5057165a7f2
João Pedro Alvarenga <joaopedroaats@pm.me>
2020-06-20 03:35:45 -0300

    feat: add hashtags

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,34 @@
 const config = {
   subreddit: ["ProgrammerHumor"],
+  hashtags: [
+    "programmer", // 1.867
+    "programming", // 3.131
+    "developer",
+    "technology",
+    "code",
+    "coder",
+    "",
+    "programmerhumor ",
+    "codingmemes", // 75
+    "codingfun",
+    "programmingmemes",
+    "",
+    "webdesign",
+    "webdeveloper",
+    "webdevelopment",
+    "softwaredeveloper",
+    "",
+    "javascript",
+    "php",
+    "html",
+    "java",
+    "python",
+    "css",
+    "",
+    "coding", // 2.682
+    "codingjokes", //35
+    "",
+  ],
 };
 
 export default config;
